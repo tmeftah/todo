573a3347a281b033cb2704b8fe6243f5c36fd49b
tmeftah <sudtechnics@gmail.com>
2023-04-09 23:56:06 +0200

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/static/',
+  publicDir: 'assets/static/',
+  build: {
+    manifest: true,
+    outDir: 'public/static/'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
