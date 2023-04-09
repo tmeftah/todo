68db8b70ce6dd7b8e2bedd704ffb36695508a433
tmeftah <sudtechnics@gmail.com>
2023-04-10 00:20:31 +0200

    fix pwa 3

--- public/app.js
+++ public/app.js
@@ -1,22 +1,22 @@
-window.onload = () => {
-  'use strict'
-
+const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js', {
+        scope: '/'
+      })
+      if (registration.installing) {
+        console.log('Service worker installing')
+      } else if (registration.waiting) {
+        console.log('Service worker installed')
+      } else if (registration.active) {
+        console.log('Service worker active')
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`)
+    }
   }
 }
 
-// const demoBtn = document.querySelector(".demo");
+// …
 
-// demoBtn.style.display = "block";
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  // Stash the event so it can be triggered later.
-  // Update UI to notify the user they can add to home screen
-  // demoBtn.style.display = "none";
-})
-
-navigator.serviceWorker.addEventListener('controllerchange', function () {
-  window.location.reload()
-})
+registerServiceWorker()
