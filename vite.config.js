78c78912196fdc685a5ea0153a59bd4ea3bd0c85
tmeftah <sudtechnics@gmail.com>
2023-04-10 01:02:45 +0200

    fix 6

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  publicDir: 'public',
-  build: {
-    manifest: true,
-    outDir: 'dist/'
-  },
   plugins: [vue()],
   resolve: {
     alias: {
